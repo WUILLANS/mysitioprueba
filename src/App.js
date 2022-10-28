2457e5cf12774df41a9f1a8c00954fbc59cad921
Wuillans <capo20033@gmail.com>
2022-10-27 19:09:21 -0500

    V01 MY-APP1 Proyecto React solo tiene mi nombre

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hola mi sitio react WUILLANS
         </a>
       </header>
     </div>

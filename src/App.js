6f2bdd3ccae8b6166ecdc34dbbd5b353739de2b9
Wuillans <capo20033@gmail.com>
2022-11-03 18:34:15 -0500

    v1 he borrado los estilos del archivo app.js. el app j.s fue modificado y ahora tiene solo el saludo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hola mi sitio react WUILLANS URQUIZO
-        </a>
-      </header>
+    <div>
+      hola mundo como estan
     </div>
   );
 }
